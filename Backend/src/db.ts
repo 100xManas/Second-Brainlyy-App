9393d1339a04958529b27678e1d1c647a21bb2c5
Manas <manasranjansahoo971@gmail.com>
2025-04-22 22:57:15 +0530

    Write Schema.

---
+++ Backend/src/db.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: true },
+    createdAt: { type: Date, default: Date.now }
+})
+
+const noteSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String },
+    link: { type: String },
+    imageUrl: { type: String },
+    type: { type: String, enum: ['tweet', 'image', 'video', 'article', "docs", "link"], required: true },
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag", }],
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+}, { timestamps: true })
+
+const linkSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    noteId: { type: mongoose.Schema.Types.ObjectId, ref: "Note", required: true },
+    url: String,
+})
+
+const tagSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true }
+})
+
+const userModel = mongoose.model("User", userSchema)
+const noteModel = mongoose.model("Note", noteSchema)
+const linkModel = mongoose.model("Link", linkSchema)
+const tagModel = mongoose.model("Tag", tagSchema)
+
+export default { userModel, noteModel, linkModel, tagModel };
